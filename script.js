db1fe2772d8240b6ca3750788ab779935a1e0032
Ryan Ameri <ryan@internettradie.com.au>
2020-05-19 13:12:09 +1000

    implemented darkening pen feature

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const containerHeight = 960;
 const containerWidth = 960;
 const container = document.getElementById("container");
 let gridNum = 16;
-let bgColor = "white";
+let bgColor = "#FFFFFF";
 let ptColor = getRandomColor();
 let ptRandom = true;
+let ptDarken = false;
 let rdGrid = false;
 let borderSize = 1;
 
-drawContainer(container, containerHeight, containerWidth, gridNum);
-drawGrid(container, gridNum, bgColor, ptColor);  
+drawContainer();
+drawGrid();  
 
 //Ask for a new grid size and create a new grid based on that size
 const newButton = document.getElementById("new");
@@ -24,8 +25,8 @@ newButton.addEventListener('click', (e) => {
         else {
             gridNum = tempGrid;
             clearContainer(container);
-            drawContainer(container, containerHeight, containerWidth, gridNum);
-            drawGrid(container, gridNum, bgColor, ptColor);   
+            drawContainer();
+            drawGrid();   
         }
     }
     else {
@@ -36,17 +37,20 @@ newButton.addEventListener('click', (e) => {
 const bgColorPicker = document.getElementById("bgcolor");
 bgColorPicker.addEventListener('change', (e) => {
     bgColor = bgColorPicker.value;
+    if (ptDarken){
+        ptColor = pSBC(-0.1, bgColor);
+    }
     clearContainer(container);
-    drawContainer(container, containerHeight, containerWidth, gridNum);
-    drawGrid(container, gridNum, bgColor, ptColor);   
+    drawContainer();
+    drawGrid();   
 });
 
 
 const clearButton = document.getElementById("clear");
 clearButton.addEventListener('click', (e) => {
     clearContainer(container);
-    drawContainer(container, containerHeight, containerWidth, gridNum);
-    drawGrid(container, gridNum, bgColor, ptColor);   
+    drawContainer();
+    drawGrid();   
 });
 
 
@@ -55,17 +59,25 @@ clearButton.addEventListener('click', (e) => {
 //Set pointer color (change the value of ptRandom if it is not random)
 const pointerColor = document.getElementById("ptcolor");
 pointerColor.addEventListener('change', (e) => {
-    if (pointerColor.value !== 'random'){
+    if (pointerColor.value === 'random'){
+        ptRandom = true;
+        ptDarken = false;
+        ptColor = getRandomColor();
+    }
+    else if (pointerColor.value === 'darken'){
+        ptDarken = true;
+        ptRandom = false;
+        ptColor = pSBC(-0.1, bgColor);
+
+    }
+    else {
+        ptDarken = false;
         ptRandom = false;
         ptColor = pointerColor.value;
     }
-    else {
-        ptRandom = true;
-        ptColor = getRandomColor();
-    }
     clearContainer(container);
-    drawContainer(container, containerHeight, containerWidth, gridNum);
-    drawGrid(container, gridNum, bgColor, ptColor);   
+    drawContainer();
+    drawGrid();   
 });
 
 
@@ -81,16 +93,16 @@ randomGrid.addEventListener('change', (e) => {
         bgColorPicker.disabled = false;
     }
     clearContainer(container);
-    drawContainer(container, containerHeight, containerWidth, gridNum);
-    drawGrid(container, gridNum, bgColor, ptColor);   
+    drawContainer();
+    drawGrid();   
 });
 
 const borderSizePicker = document.getElementById("bordersize");
 borderSizePicker.addEventListener('change', (e) => {
     borderSize = borderSizePicker.value;
     clearContainer(container);
-    drawContainer(container, containerHeight, containerWidth, gridNum);
-    drawGrid(container, gridNum, bgColor, ptColor);   
+    drawContainer();
+    drawGrid();   
 });
 
 function clearContainer(container){
@@ -98,7 +110,7 @@ function clearContainer(container){
 
 }
  
- function drawContainer(container, containerHeight, containerWidth, gridNum){
+ function drawContainer(){
     
     const containerHeightString = containerHeight + "px";
     const containerWidthString = containerWidth + "px";
@@ -118,7 +130,7 @@ function clearContainer(container){
     container.style.gridRowGap = borderSizeString;
  }
 
- function drawGrid(container, gridNum, bgColor, ptColor){
+ function drawGrid(){
     let grid = [];
     
     for (let i = 0; i < gridNum * gridNum; i++){
@@ -133,6 +145,12 @@ function clearContainer(container){
         if (ptRandom) {
             ptColor = getRandomColor();
         }
+
+        if (ptDarken){
+            ptColor = pSBC(-0.1, grid[i].style.backgroundColor);
+            
+        }
+
         grid[i].style.backgroundColor = ptColor;
         grid[i].style.cursor = "cell";
         });
@@ -152,3 +170,29 @@ function clearContainer(container){
     return color;
   }
 
+// function from https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
+// Version 4.0
+const pSBC=(p,c0,c1,l)=>{
+    let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
+    if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+    if(!this.pSBCr)this.pSBCr=(d)=>{
+        let n=d.length,x={};
+        if(n>9){
+            [r,g,b,a]=d=d.split(","),n=d.length;
+            if(n<3||n>4)return null;
+            x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+        }else{
+            if(n==8||n==6||n<4)return null;
+            if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+            d=i(d.slice(1),16);
+            if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+            else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+        }return x};
+    h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+    if(!f||!t)return null;
+    if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+    else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+    a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+    if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+    else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+}
